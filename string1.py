99dba5c2374ff2965cfc9bc9a28e33d54df3e539
student <student@INFORMATYKA.local>
2017-03-01 08:32:32 +0100

    zmiany w plikach

--- string1.py
+++ string1.py
@@ -25,7 +25,7 @@
 # and donuts(23) returns 'Number of donuts: many'
 def donuts(count):
   if count <=10:
-    return "Number of donuts" + count
+    return "Number of donuts" + str (count)
   else:
     return "Number of donuts: many"
 
@@ -65,8 +65,9 @@ def fix_start(s):
 #   'dog', 'dinner' -> 'dig donner'
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
-  # +++your code here+++
-  return
+  a_s = b[:2] + b[2:]
+  b_s = a[:2] + b[2:]
+  return a_s + '' + b_s
 
 
 # Provided simple test() function used in main() to print
@@ -76,36 +77,36 @@ def test(got, expected):
     prefix = ' OK '
   else:
     prefix = '  X '
-  print '%s got: %s expected: %s' % (prefix, repr(got), repr(expected))
+  print ('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
 
 
 # Provided main() calls the above functions with interesting inputs,
 # using test() to check if each result is correct or not.
 def main():
-  print 'donuts'
-  # Each line calls donuts, compares its result to the expected for that call.
+  print ('donuts')
+  
   test(donuts(4), 'Number of donuts: 4')
   test(donuts(9), 'Number of donuts: 9')
   test(donuts(10), 'Number of donuts: many')
   test(donuts(99), 'Number of donuts: many')
 
-  print
-  print 'both_ends'
+  
+  print ('both_ends')
   test(both_ends('spring'), 'spng')
   test(both_ends('Hello'), 'Helo')
   test(both_ends('a'), '')
   test(both_ends('xyz'), 'xyyz')
 
   
-  print
-  print 'fix_start'
+ 
+  print ('fix_start')
   test(fix_start('babble'), 'ba**le')
   test(fix_start('aardvark'), 'a*rdv*rk')
   test(fix_start('google'), 'goo*le')
   test(fix_start('donut'), 'donut')
 
-  print
-  print 'mix_up'
+  
+  print ('mix_up')
   test(mix_up('mix', 'pod'), 'pox mid')
   test(mix_up('dog', 'dinner'), 'dig donner')
   test(mix_up('gnash', 'sport'), 'spash gnort')
